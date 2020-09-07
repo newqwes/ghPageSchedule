5d88069c76a70098dcec667cf01656a87c258b95
Nazar Ashirov <newnewqwes@gmail.com>
2020-09-08 00:11:00 +0300

    page

---
+++ precache-manifest.5d947bcc8754205fb2ca6ce5312ffb31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f8b7100290e22fcaac59be148bf558",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4259968206b0dcf0137b",
+    "url": "/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "62a6936f4f119eb6861d",
+    "url": "/static/css/main.e1732af4.chunk.css"
+  },
+  {
+    "revision": "4259968206b0dcf0137b",
+    "url": "/static/js/2.b1e5b21f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b1e5b21f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62a6936f4f119eb6861d",
+    "url": "/static/js/main.626082e0.chunk.js"
+  },
+  {
+    "revision": "03d5d6d79e167aebe061",
+    "url": "/static/js/runtime-main.e04f0a67.js"
+  }
+]);
